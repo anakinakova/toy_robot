99d1277b8db7290e351490687b9371ec30ad8e70
Ana Djordjevic <ana@oomphhq.com>
2014-10-15 23:12:35 +1000

    Bit of refactor.
    - error checking
    - input parsing
    - allow for input file and output file

--- bin/toy_robot.rb
+++ bin/toy_robot.rb
@@ -1,14 +1,11 @@
 #! /usr/bin/env ruby
 require ::File.expand_path('../../lib/toy_robot',  __FILE__)
 
-def process_file(robot, filename)
-  file = File.open(filename, 'r')
-  unless file
-    print "ERROR", "Unable to open file #{file}.\n"
-    return
-  end
-  file.each_line do |line|
+def process_input(robot)
+  input = robot.input
+  input.each_line do |line|
     execute_command(robot, parse_command(line))
+    print "<robot> " if input == STDIN
   end
 end
 
@@ -37,21 +34,40 @@ def execute_command(robot, args)
   end
 end
 
-print "Toy Robot Simulator\n"
-print "-- Ana Djordjevic, 2014\n\n"
+def parse_args(args)
+  i = args.find_index("-i")
+  if i.nil?
+    input = STDIN
+  else
+    filename = args[i + 1]
+    input = File.open(filename, 'r')
+    if input.nil?
+      print "ERROR", "Unable to open file #{filename}.\n"
+      print "Falling back to interactive mode.\n"
+    end
+  end
 
-robot = ToyRobot::Robot.new
+  o = args.find_index("-o")
+  if o.nil?
+    output = STDOUT
+  else
+    filename = args[o + 1]
+    output = File.open(filename, 'w')
+    if output.nil?
+      print "ERROR", "Unable to open file #{filename}.\n"
+      print "Writing output to standard output.\n"
+    end
+  end
 
-unless ARGV[0].nil?
-  process_file(robot, ARGV[0])
-  exit 0
+  [input, output]
 end
 
-print "Interactive mode - enter commands one line at a time:\n"
-print "Ctrl-D to exit.\n\n"
-print "<robot> "
-
-STDIN.each_line do |line|
-  execute_command(robot, parse_command(line))
+robot = ToyRobot::Robot.new(*parse_args(ARGV))
+if robot.input == STDIN
+  print "Toy Robot Simulator\n"
+  print "-- Ana Djordjevic, 2014\n\n"
+  print "Interactive mode - enter commands one line at a time:\n"
+  print "Ctrl-D to exit.\n\n"
   print "<robot> "
 end
+process_input(robot)
